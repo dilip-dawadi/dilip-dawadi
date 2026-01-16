99e43ff3a30450f06797de6809b28383dadad7c2
Dilip-Dawadi <zanzerdawadi123@gmail.com>
2026-01-16 00:36:39 -0500

    refactor: remove admin access check and redirect from DashboardLayout

--- app/admin/dashboard/layout.tsx
+++ app/admin/dashboard/layout.tsx
@@ -1,14 +1,3 @@
-import { checkAdminAccess } from '@/lib/admin';
-import { redirect } from 'next/navigation';
-
-export const dynamic = 'force-dynamic';
-
-export default async function DashboardLayout({ children }: { children: React.ReactNode }) {
-  const { isAdmin } = await checkAdminAccess();
-  console.log('Is Admin:', isAdmin);
-  if (!isAdmin) {
-    redirect('/admin');
-  }
-
+export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return <>{children}</>;
 }
